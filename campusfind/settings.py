d1365fd45e00cb3e800f86780637e07d1db7d57b
hackura <karlseyram18@gmail.com>
2026-08-17 13:06:02 +0100

    fix(settings): use regional S3 domain for direct image delivery

--- campusfind/settings.py
+++ campusfind/settings.py
@@ -179,7 +179,7 @@ if USE_S3 and os.getenv('AWS_STORAGE_BUCKET_NAME'):
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'us-east-1')
-    AWS_S3_CUSTOM_DOMAIN = os.getenv('AWS_S3_CUSTOM_DOMAIN', f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com')
+    AWS_S3_CUSTOM_DOMAIN = os.getenv('AWS_S3_CUSTOM_DOMAIN', f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com')
     AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = None  # Bucket owner enforced
     AWS_QUERYSTRING_AUTH = False
